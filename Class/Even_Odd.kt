f9505de30be6884556bc177cc76aef07560fc971
Namit <namityadav008@gmail.com>
2024-08-27 11:16:05 +0530

    questions

---
+++ Class/Even_Odd.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val arr = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    var sumEven = 0
+    var sumOdd = 0
+
+    for (num in arr) {
+        if (num % 2 == 0) {
+            sumEven += num
+        } else {
+            sumOdd += num
+        }
+    }
+
+    println("Sum of even numbers: $sumEven")
+    println("Sum of odd numbers: $sumOdd")
+}
